a84496151b8db5bbaece3f18e16c7a20ec078e3b
Fariya Islam <62393112+fariyaislam123@users.noreply.github.com>
2022-02-06 19:56:13 +0600

    Initial Commit

---
+++ UTest/UTest/Controllers/HomeController.cs
@@ -0,0 +1,115 @@
+﻿using UTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+namespace UTest.Controllers
+{
+    public class HomeController : Controller
+    {       Login2 db =new Login2();
+
+        /* public ActionResult Signup()
+         {
+
+             return View();
+         }
+
+         [HttpPost]
+         public ActionResult Signup(User us)
+         {
+             db.Users.Add(us);
+             db.SaveChangesAsync();
+            // db.SaveChanges();
+             return RedirectToAction("Login");
+         }
+         [HttpGet]
+         public ActionResult Login()
+         {
+
+             return View();
+         }
+
+         [HttpPost]
+         public ActionResult Login(User us)
+         {
+             var obj=db.Users.Where(x => x.Email.Equals(us.Email) && x.Password.Equals(us.Password)).FirstOrDefault();
+             if (obj != null)
+             {
+                 return RedirectToAction("Dashboard");
+             }
+            return View();
+         }
+         public ActionResult Dashboard()
+         {
+
+             return View();
+         }*/
+        [HttpGet]
+        public ActionResult Signup()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(User us)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(us);
+                db.SaveChangesAsync();
+            }
+           
+            return RedirectToAction("Login");
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(User us)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                db.Users.Add(us);
+          
+                db.SaveChanges();
+               
+            }
+            return RedirectToAction("Login");
+        }
+
+        [HttpGet]
+         public ActionResult Login()
+         {
+
+             return View();
+         }
+
+         [HttpPost]
+         public ActionResult Login(User us)
+         {
+             var obj=db.Users.Where(x => x.Email.Equals(us.Email) && x.Password.Equals(us.Password)).FirstOrDefault();
+             if (obj != null)
+             {
+                 return RedirectToAction("Dashboard");
+             }
+            return View();
+         }
+         public ActionResult Dashboard()
+         {
+
+             return View();
+         }
+
+    }
+}
---
+++ UTest/UTest/js/question.js
@@ -0,0 +1,24 @@
+let questions = [
+  {
+    number: 1,
+    question: "What does html Stands for ?",
+    answer: "HyperText markup languege",
+    options: [
+      "HyperText markup languege",
+      "hu mu lalala ",
+      "ami jani na",
+      "ami bolbo na",
+    ],
+  },
+  {
+    number: 2,
+    question: "What does CSS Stands for ?",
+    answer: "Cascading Style Sheets",
+    options: [
+      "Cascading Style Sheets",
+      "cholo sokal sokal",
+      "ami jani na",
+      "ami bolbo na",
+    ],
+  },
+];
