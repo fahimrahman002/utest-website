7366fec43a1eb930abdc301c84cf06fbcc83afa5
Fariya Islam <62393112+fariyaislam123@users.noreply.github.com>
2022-02-07 09:54:14 +0600

    Signup Complete

--- UTest/UTest/Controllers/HomeController.cs
+++ UTest/UTest/Controllers/HomeController.cs
@@ -1,115 +1,44 @@
-﻿using UTest.Models;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using UTest.Models;
 
 
 namespace UTest.Controllers
 {
     public class HomeController : Controller
-    {       Login2 db =new Login2();
-
-        /* public ActionResult Signup()
-         {
-
-             return View();
-         }
-
-         [HttpPost]
-         public ActionResult Signup(User us)
-         {
-             db.Users.Add(us);
-             db.SaveChangesAsync();
-            // db.SaveChanges();
-             return RedirectToAction("Login");
-         }
-         [HttpGet]
-         public ActionResult Login()
-         {
-
-             return View();
-         }
-
-         [HttpPost]
-         public ActionResult Login(User us)
-         {
-             var obj=db.Users.Where(x => x.Email.Equals(us.Email) && x.Password.Equals(us.Password)).FirstOrDefault();
-             if (obj != null)
-             {
-                 return RedirectToAction("Dashboard");
-             }
-            return View();
-         }
-         public ActionResult Dashboard()
-         {
-
-             return View();
-         }*/
+    {
+       DataEntities db=new DataEntities();
         [HttpGet]
-        public ActionResult Signup()
+        public ActionResult SignUp()
         {
+
 
             return View();
         }
-
         [HttpPost]
-        public ActionResult Signup(User us)
+        public ActionResult SignUp(User us)
         {
-            if (ModelState.IsValid)
-            {
+            
+            
                 db.Users.Add(us);
-                db.SaveChangesAsync();
-            }
+                db.SaveChanges();
+            
            
             return RedirectToAction("Login");
         }
+
         [HttpGet]
-        public ActionResult Create()
+        public ActionResult Login()
         {
 
             return View();
         }
-        [HttpPost]
+
         
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(User us)
-        {
-            if (ModelState.IsValid)
-            {
-               
-                db.Users.Add(us);
-          
-                db.SaveChanges();
-               
-            }
-            return RedirectToAction("Login");
-        }
-
-        [HttpGet]
-         public ActionResult Login()
-         {
-
-             return View();
-         }
-
-         [HttpPost]
-         public ActionResult Login(User us)
-         {
-             var obj=db.Users.Where(x => x.Email.Equals(us.Email) && x.Password.Equals(us.Password)).FirstOrDefault();
-             if (obj != null)
-             {
-                 return RedirectToAction("Dashboard");
-             }
-            return View();
-         }
-         public ActionResult Dashboard()
-         {
-
-             return View();
-         }
 
     }
 }
