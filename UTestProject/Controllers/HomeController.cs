373854ab81011d0e36bb5c51830ab3cfe865d16e
Fahim Rahman <57598021+fahimrahman002@users.noreply.github.com>
2022-02-28 09:20:17 +0600

    user login signup bug fixed

--- UTestProject/Controllers/HomeController.cs
+++ UTestProject/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,74 +11,21 @@ namespace UTestProject.Controllers
 {
     public class HomeController : Controller
     {
-        UTestDBEntities1 studentDb = new UTestDBEntities1();
-        UTestDBEntities2 categoryDb = new UTestDBEntities2();
+
         public ActionResult Index()
         {
-            return View();
-        }
-        [HttpGet]
-        public ActionResult Login()
-        {
-            return View();
-        }
-
-        [ActionName("Login")]
-        [HttpPost]
-        public ActionResult LoginForm()
-        {
-            string email = Request.Form["emailInput"];
-            string password = Request.Form["passwordInput"];
-            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            if (Session["UserEmail"] != null && Session["UserPass"] != null)
             {
-                Response.Redirect(Request.RawUrl);
-            }
-
-            List<Student> students = studentDb.Student.Where(temp => temp.Email.Equals(email) && temp.Password.Equals(password)).ToList();
-            if (students.Count == 0)
-            {
-                Response.Redirect(Request.RawUrl);
+                return RedirectToAction("Dashboard", "User");
             }
             else
             {
-                return RedirectToAction("Dashboard");
+                return View();
+
             }
 
-            return View();
-
-        }
-        [HttpGet]
-        public ActionResult SignUp()
-        {
-            List<Category> categories = categoryDb.Category.ToList();
-
-            return View(categories);
         }
 
-        [ActionName("SignUp")]
-        [HttpPost]
-        public ActionResult SignUpForm()
-        {
-            string name = Request.Form["nameInput"];
-            string email = Request.Form["emailInput"];
-            string password = Request.Form["passwordInput"];
-            string category = Request.Form["categoryInput"];
-            Student student = new Student() { Name = name, Email = email, Password = password, Category = category };
-            studentDb.Student.Add(student);
-            studentDb.SaveChanges();
-            return RedirectToAction("Login");
-        }
-
-        [HttpGet]
-        public ActionResult Dashboard()
-        {
-            return View();
-        }
-
-        public ActionResult Logout()
-        {
-            return RedirectToAction("Login");
-        }
 
 
     }
--- UTestProject/Controllers/UserController.cs
+++ UTestProject/Controllers/UserController.cs
@@ -1,23 +1,141 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UTestProject.Models;
 
 namespace UTestProject.Controllers
 {
     public class UserController : Controller
     {
-        // GET: User Profile
-        public ActionResult Profile()
+        UTestDBEntities1 studentDb = new UTestDBEntities1();
+        UTestDBEntities2 categoryDb = new UTestDBEntities2();
+
+        [HttpGet]
+        public ActionResult Login()
         {
             return View();
+        }
+
+        [ActionName("Login")]
+        [HttpPost]
+        public ActionResult LoginForm()
+        {
+            string email = Request.Form["emailInput"];
+            string password = Request.Form["passwordInput"];
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+
+                Response.Redirect(Request.RawUrl);
+            }
+
+            List<Student> students = studentDb.Student.Where(temp => temp.Email.Equals(email) && temp.Password.Equals(password)).ToList();
+            if (students.Count == 0)
+            {
+                Response.Redirect(Request.RawUrl);
+            }
+            else
+            {
+                Session["UserName"] = students[0].Name;
+                Session["UserEmail"] = email;
+                Session["UserPass"] = password;
+                return RedirectToAction("Dashboard");
+            }
+
+            return View();
+
+        }
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            List<Category> categories = categoryDb.Category.ToList();
+
+            return View(categories);
+        }
+
+        [ActionName("SignUp")]
+        [HttpPost]
+        public ActionResult SignUpForm()
+        {
+
+            string name = Request.Form["nameInput"];
+            string email = Request.Form["emailInput"];
+            string password = Request.Form["passwordInput"];
+            string category = Request.Form["categoryInput"];
+            Student student = new Student() { Name = name, Email = email, Password = password, Category = category };
+            try
+            {
+                studentDb.Student.Add(student);
+                studentDb.SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+                if (ex.InnerException?.InnerException is SqlException sqlEx &&
+    (sqlEx.Number == 2601 || sqlEx.Number == 2627))
+                {
+                    List<Category> categories = categoryDb.Category.ToList();
+                    ViewData["MessageText"] = "Email already exists.";
+                    ViewData["MessageType"] = "error";
+                    return View(categories);
+
+                }
+
+            }
+
+            return RedirectToAction("Login");
+        }
+        public ActionResult Logout()
+        {
+            Session.RemoveAll();
+            return RedirectToAction("Login");
+        }
+
+        public new ActionResult Profile()
+        {
+            if (Session["UserEmail"] != null && Session["UserPass"] != null)
+            {
+                return View();
+            }
+            else
+            {
+
+                return RedirectToAction("Login");
+            }
+
         }
 
         // GET: User settings
         public ActionResult Settings()
         {
-            return View();
+            if (Session["UserEmail"] != null && Session["UserPass"] != null)
+            {
+                return View();
+            }
+            else
+            {
+
+                return RedirectToAction("Login");
+            }
+
         }
+
+        [HttpGet]
+        public ActionResult Dashboard()
+        {
+            if (Session["UserEmail"] != null && Session["UserPass"] != null)
+            {
+                return View();
+            }
+            else
+            {
+
+                return RedirectToAction("Login");
+            }
+
+        }
+
     }
 }
